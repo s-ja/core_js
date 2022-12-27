1f20c8a28ef66efc37814e874ccad865bf093080
s-ja <dkstmdwl0615@naver.com>
2022-12-27 13:43:27 +0900

    [생성] 환경설정 구성

---
+++ server/index.js
@@ -0,0 +1,16 @@
+console.log('Have a Nice Trip');
+// console.log(window);
+console.log(global);
+
+// js 가 작동할 수 있는 환경 - 브라우저와 노드
+
+const liveServer = require('live-server');
+
+const params = {
+    host: 'localhost',
+    port: '3000',
+    open: 'false',
+    root: './client'
+}
+
+liveServer.start(params);
