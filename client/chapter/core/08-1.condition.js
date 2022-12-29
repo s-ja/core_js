5703ddf936bb1ed34705f7d855d1cd9dc55043d7
s-ja <dkstmdwl0615@naver.com>
2022-12-29 13:18:13 +0900

    condition complete, logical start

--- client/chapter/core/08-1.condition.js
+++ client/chapter/core/08-1.condition.js
@@ -2,7 +2,7 @@
 /* Condition                                                              */
 /* ---------------------------------------------------------------------- */
 
-let number = prompt('숫자를 입력하세요',0)
+// let number = prompt('숫자를 입력하세요',0)
 
 // if(number>0){
 //     console.log(1);
@@ -14,8 +14,8 @@ let number = prompt('숫자를 입력하세요',0)
 //     console.log("말좀 들어");
 // }
 
-let message = (number>0) ? '1' : (number<0) ? '-1' : '말좀 들으라니까?'
-console.log(message);
+// let message = (number>0) ? '1' : (number<0) ? '-1' : '말좀 들으라니까?'
+// console.log(message);
 
 
 
@@ -28,18 +28,28 @@ console.log(message);
 //       Yes | No
 
 // 영화 봤니?
-let didWatchMovie;
+let didWatchMovie = 'yes';
 
 // 영화 볼거니?
-let goingToWatchMovie;
-
+let goingToWatchMovie = 'no';
 
 // if 문(statement)
-
-// else 절(caluse)
-
+if(didWatchMovie.includes('yes')){
+// if(didWatchMovie === 'yes'){
+    console.log('재밌었니');
+}
 // else if 복수 조건 처리
-
+else if(goingToWatchMovie === 'yes'){
+    console.log('뭐 볼꺼니');
+}
+// else 절(caluse)
+else{
+   console.log('그럼 뭐 할꺼니?');
+}
 // 조건부 연산자
+let movieMessage = (didWatchMovie.includes('yes')) ? '재밌었니' : 
+(goingToWatchMovie === 'yes') ? '뭐 볼꺼니' : '그럼 뭐 할꺼니';
+
+console.log(movieMessage);
 
 // 멀티 조건부 연산자 식
