61d6e3c82b1150bb0ba2ab1fc709909ed10c82a6
s-ja <dkstmdwl0615@naver.com>
2022-12-29 14:48:00 +0900

    logical complete, switch started

--- client/chapter/core/08-2.logical.js
+++ client/chapter/core/08-2.logical.js
@@ -1,5 +1,30 @@
+// ! 첫번째 truthy 는 or 연산자가 찾고,
+// ! 첫번째 falsy 는 and 연산자가 찿는다.
+// ! not > and > or
+// console.log( alert(1) || 2 || alert(3) );
+// alert 는 종료후 undefined 를 반환
+
 /* ---------------------------------------------------------------------- */
 /* Logical Operators                                                      */
+/* ---------------------------------------------------------------------- */
+let age = 28
+if (age >= 14 && age <= 90)
+
+// if(age>=14){
+//     console.log('14 over');
+// }else if(age<=90){
+//     console.log('90 under');
+// }else{'...'}
+
+if ((age >= 14) && (age <= 90)) {   
+console.log("나이에 속합니다."); 
+} else {   
+console.log("나이에 속하지 않습니다.") 
+}
+
+// let age_ = prompt("나이를 입력하세요", 25);
+// if (age_ >= 14 && age <= 90) { console.log("맞다!"); } else { console.log("너 몇살이냐?"); }
+
 /* ---------------------------------------------------------------------- */
 
 let a = 10;
@@ -7,19 +32,49 @@ let b = '';
 let value = Boolean(b);
 
 // 논리곱(그리고) 연산자
-let AandB;
-
+let AandB = a&&b;
+console.log(AandB);
 // 논리합(또는) 연산자
-let AorB;
-
+let AorB = a||b;
+console.log(AorB);
 // 부정 연산자
-let reverseValue;
-
+let reverseValue = !value;
 
 // 조건 처리
 
 // 첫번째 Falsy를 찾는 연산 (&&)
-let whichFalsy;
-
+let whichFalsy = true && ' ' && [] && {} &&{thisIsFalsy:false};
+// true && true && true && true -> 마지막 값 반환 : 비어있어도 배열이라는 값이 들어 있기 때문에 true
+console.log(whichFalsy);
 // 첫번째 Truthy를 찾는 연산 (||)
-let whichTruthy;
+let whichTruthy = false || '' || [2,3].length || {thisIsTruthy:true};
+console.log([2,3].length,'[2,3].length = 2');
+console.log(whichTruthy);
+
+let userName = prompt('userName','userName') ;
+// console.log(userName);
+let pw ;
+
+// if (userName === 'Admin' && pw===123 ){
+//     alert('welcome')
+// }else if(userName ==='Admin' || pw===123){
+//     alert('check pw')
+// }else{alert('get out')}
+
+if(userName === 'Admin'){
+    let pw = prompt('password','password');
+    if(pw === '123'){
+        console.log('welcome');
+    }else if(pw === '' || pw === null){
+        console.log('cancled');
+    }else{
+        console.log('get out');
+    }
+    // console.log('checked');
+}else if(userName === '' || userName === null){
+    console.log('cancled');
+}else{
+    console.log("can't check");
+}
+
+//! 대소문자 구분 없이 입력 받아도 로그인이 되게끔?
