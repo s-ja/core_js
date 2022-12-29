c6a29b3ac6cf6dbb7c9a7e8f14c949f8fc73018f
s-ja <dkstmdwl0615@naver.com>
2022-12-29 11:52:46 +0900

    조건문 진행중

---
+++ client/chapter/core/08-1.condition.js
@@ -0,0 +1,45 @@
+/* ---------------------------------------------------------------------- */
+/* Condition                                                              */
+/* ---------------------------------------------------------------------- */
+
+let number = prompt('숫자를 입력하세요',0)
+
+// if(number>0){
+//     console.log(1);
+// }else if(number<0){
+//     console.log(-1);
+// }else if(number == 0){
+//     console.log(0);
+// }else{
+//     console.log("말좀 들어");
+// }
+
+let message = (number>0) ? '1' : (number<0) ? '-1' : '말좀 들으라니까?'
+console.log(message);
+
+
+
+
+// 그 영화 봤니?
+//     ↓
+// Yes | No
+//     | 영화 볼거니?
+//           ↓
+//       Yes | No
+
+// 영화 봤니?
+let didWatchMovie;
+
+// 영화 볼거니?
+let goingToWatchMovie;
+
+
+// if 문(statement)
+
+// else 절(caluse)
+
+// else if 복수 조건 처리
+
+// 조건부 연산자
+
+// 멀티 조건부 연산자 식
