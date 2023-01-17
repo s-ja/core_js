38b009762ebabd710e00c0fbcbc898ffbe0f9203
s-ja <dkstmdwl0615@naver.com>
2023-01-17 16:12:58 +0900

    utils/delay.js 생성 23.01.17 수업종료, promise 개념 학습중 종료

--- client/lib/utils/delay.js
+++ client/lib/utils/delay.js
@@ -21,6 +21,10 @@ delay(()=>{
 })
 
 
-// first.style.transform = 'rotate(360deg)' // 동시에 작동 -> 콜백의 필요성
+// first.style.transform = 'rotate(360deg)' // 동시에 작동 -> 콜백의 필요성 & 콜백지옥
 
 // first.style.top = '0px'
+
+//! 프라미스 = 오로지 콜백의 가독성만을 위해.
+
+
