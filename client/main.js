ef4eba546e5138644c9695e36040e81353dfc901
s-ja <dkstmdwl0615@naver.com>
2023-01-18 17:13:53 +0900

    23.01.18 수업 종료

--- client/main.js
+++ client/main.js
@@ -7,7 +7,7 @@ async function render(){
     
     await delayP(2000);
     let response =  await tiger.get('https://jsonplaceholder.typicode.com/users/1')
-    // console.log(response.data);
+    console.log(response.data);
 }
 
 console.log(render());
