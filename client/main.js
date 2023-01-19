e2ed28401bc5ddae4789d5149d4a840c69a7ad36
s-ja <dkstmdwl0615@naver.com>
2023-01-19 16:28:15 +0900

    23.01.19 js수업 마지막, 토이프로젝트 종료

--- client/main.js
+++ client/main.js
@@ -1,5 +1,24 @@
 
 
-import {  } from "./lib/index.js";
+import { getNode, loadStorage, saveStorage } from "./lib/index.js";
 
 
+const textField = getNode('#textField');
+const deleteButton = getNode('input[value="삭제"]');
+
+
+
+loadStorage('area').then((res)=>{
+    textField.value = res;
+})
+
+
+
+
+function inputHandler() {
+    saveStorage('area',textField.value);
+}
+
+
+
+textField.addEventListner('input',inputHandler)
